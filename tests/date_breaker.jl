1fffe1c239b5091deae1c8daa6fcd21f4440f354
Karl <karl.friesen@invenia.ca>
2016-04-01 14:23:02 -0500

    updating

--- tests/date_breaker.jl
+++ tests/date_breaker.jl
@@ -1,42 +1,45 @@
-using PyCall
 using StackTraces
 
-(Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday) = (Dates.Monday, Dates.Tuesday, Dates.Wednesday, Dates.Thursday, Dates.Friday, Dates.Saturday, Dates.Sunday)
-
 function datebreaker()
+    # date = Date(2018, 1, 1)
     date = Date(2018, 1, 1)
-    date_name = ""
+    last_weekday = Dates.Monday
 
-#~     try
+    x = UInt64(0)
+
+    try
         # Loop until failure.
         while true
             # Loop through dates and weekdays
-            for weekday in (Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday)
-                # Occasional print statements just so you can monitor how long this has been going mostly.
-                println("Testing weekday",weekday)
+            # for weekday in (Dates.Monday, Dates.Tuesday, Dates.Wednesday, Dates.Thursday, Dates.Friday, Dates.Saturday, Dates.Sunday)
+            for weekday in (Dates.Sunday, Dates.Monday, Dates.Saturday, Dates.Friday, Dates.Tuesday, Dates.Wednesday, Dates.Thursday)
+                last_weekday = weekday
 
-                date = Date(2018, 1, 1)
-                last_date = Date(2022, 1, 1)
+                date = Date(1967, 3, 9)
+                last_date = Date(2024, 4, 2)
 
                 while date < last_date
                     # This call is where the error will be raised from first...
                     temp = Dates.tonext(x->Dates.dayofweek(x) == weekday, date)
 
+                    x = x + 1
+
                     date = date + Dates.Day(1)
                 end
             end
         end
-#~     catch e
-#~         # Help diagnose the error by printing as much stuff as possible.
-#~         whos()
-#~         println(stacktrace())
-#~         println("Error",e)
-#~         println("Last date tried:",date)
-#~         println("Date Name is currently",date_name)
-#~         return false
-#~     end
+    catch e
+        # Help diagnose the error by printing as much stuff as possible.
+        whos()
+        println(stacktrace())
+        println("Error",e)
+        println("Last date tried:",date)
+        println("Last weekday tried:",last_weekday)
+        println("Successful Runs:",x)
+        return false
+    end
 
-#~     return true
+    return true
 end
 
 # Loop this until the error happens...
